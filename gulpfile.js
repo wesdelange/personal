69d86e84ef9ab0722fdb8ed0dcc1684d93a528ad
Wes <wes.delange@gmail.com>
2018-05-02 13:29:15 -0400

    changed path of images to minified images

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,12 @@ var gulp = require('gulp'),
     cssnano = require("gulp-cssnano"),
     autoprefixer = require("gulp-autoprefixer"),
     prettyError = require('gulp-prettyerror'),
+    imagemin = require('gulp-imagemin'),
+    cache = require('gulp-cache'),
+    del = require('del'),
+    useref = require('gulp-useref'),
+    runSequence = require('run-sequence'),
+    gulpIf = require('gulp-if'),
     rename = require('gulp-rename');
     
 gulp.task("sass", function() {
@@ -22,8 +28,33 @@ gulp.task("sass", function() {
       .pipe(rename("style.min.css"))
       .pipe(gulp.dest("./build/css"));
   });
+  
+  gulp.task('useref', function(){
+    return gulp.src('index.html')
+      .pipe(useref())
+      .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', cssnano()))
+      .pipe(gulp.dest('build'))
+  });
 
-    gulp.task('scripts', function(){
+  gulp.task('images', function(){
+    return gulp.src('./photos/*.+(png|jpg|jpeg|gif|svg)')
+    .pipe(cache(imagemin({
+        interlaced: true
+      })))
+    .pipe(gulp.dest('./build/images'))
+  });
+
+  gulp.task('fonts', function() {
+    return gulp.src('/*')
+    .pipe(gulp.dest('./build/fonts'))
+  });
+  
+  gulp.task('clean:build', function() {
+    return del.sync('build');
+  });
+   
+  gulp.task('scripts', function(){
         return gulp
         .src('./js/*.js')
           .pipe(uglify())
@@ -45,5 +76,4 @@ gulp.task("sass", function() {
     .on('change', browserSync.reload);
 });
 
-
-    gulp.task('default', gulp.parallel('browser-sync', 'watch'));
+gulp.task("default", gulp.parallel("browser-sync", "watch"));
